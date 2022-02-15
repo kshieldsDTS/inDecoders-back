a72a66673f0a10b922973ee10471c66c4ceb0f6b
kshieldsDTS <escanor05@gmail.com>
2022-02-15 10:44:15 -0500

    change models

--- users/models.py
+++ users/models.py
@@ -5,16 +5,8 @@ from django.forms import BooleanField
 
 class User(AbstractUser):
     email = models.EmailField(verbose_name='email', max_length=255,unique=True)
-    avatar = models.CharField(max_length=500)
     skills = models.CharField(max_length=500, blank=True)
     portfolio = models.CharField(max_length=500, blank=True)
-    sunday = models.BooleanField(default=False)
-    monday = models.BooleanField(default=False)
-    tuesday = models.BooleanField(default=False)
-    wednesday = models.BooleanField(default=False)
-    thursday = models.BooleanField(default=False)
-    friday = models.BooleanField(default=False)
-    saturday = models.BooleanField(default=False)
     availabilty = models.CharField(max_length=500, blank=True)
     payrate = models.IntegerField(default=0)
     REQUIRED_FIELDS = ['username']
